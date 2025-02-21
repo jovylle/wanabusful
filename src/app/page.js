793b385f1a7321a3cdb290fc835c83954b9e02c3
Code runs on coffee <git@jovylle.com>
2025-02-21 14:38:22 +0800

    remove some on home

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <Layout>
       <h1 style={{ fontSize: '2rem', fontWeight: 'bold', textAlign: 'center', marginBottom: '20px' }}>Random Utility/Mini Tools List Site</h1>
-      <p style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+      {/* <p style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
         <a href="/camera">Camera test</a>
         <a href="/signature">Signature Pad</a>
         <a href="https://github.com/jovylle/wanabusful">Github public repo link</a>
         <a href="/">Othe tools to be added</a>
-      </p>
+      </p> */}
     </Layout>
   );
 }
