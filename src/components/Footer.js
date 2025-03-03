f81d21c1e003450985ca19b039519fd0ffdb3113
Code runs on coffee <git@jovylle.com>
2025-03-03 09:46:16 +0800

    add live/beta site link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
 export default function Footer () {
+  const isBeta = typeof window !== "undefined" && window.location.hostname.includes("beta");
+  const isLive = typeof window !== "undefined" && window.location.hostname === "uft1.com";
+
   return (
     <footer className="dark:bg-gray-900 bg-gray-100 dark:text-white text-black py-4 text-center">
       <div className="container mx-auto">
@@ -26,16 +29,36 @@ export default function Footer () {
               We are a passionate team of web developers hunting for career opportunities and building dynamic, responsive websites using modern technologies.
             </p>
             <br />
+            <p>Contributors:</p>
             <p>
-              Contributors:
-            </p>
-            <p>
-              &nbsp;<a href="jovylle.com">Jovylle </a>
+              &nbsp;<a href="jovylle.com">Jovylle</a>
             </p>
           </div>
         </section>
       </div>
       <div className="container mx-auto mt-8">
+        {!isBeta && !isLive && (
+          <p>
+            <a
+              href="http://beta.uft1.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Beta Version
+            </a>
+          </p>
+        )}
+        {!isBeta && !isLive && (
+          <p>
+            <a
+              href="http://uft1.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live Version
+            </a>
+          </p>
+        )}
         <p>&copy; {new Date().getFullYear()}. All rights reserved.</p>
       </div>
     </footer>
