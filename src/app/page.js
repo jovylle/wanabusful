2e0e5a5d904181f190ab1bf05feaa511538aeb9f
Jovylle Bermudez flex5 <jovyllebermudez@gmail.com>
2025-03-08 23:26:22 +0800

    now a better left and right speaker testing

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,7 @@ export default function Home () {
             </Link>
             <Link
               href="/audio-test"
-              className="bg-purple-500 text-white px-4 py-2 rounded hover:bg-orrage-600 transition"
+              className="bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600 transition"
             >
               Audio Left/Right Speaker Test
             </Link>
